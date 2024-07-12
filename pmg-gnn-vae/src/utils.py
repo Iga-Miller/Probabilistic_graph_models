87b2d21d7c6c2cd6b4d3159b0c77aeeb68b9dd6a
Iga Miller <80620641+Iga-Miller@users.noreply.github.com>
2024-07-12 15:28:22 +0200

    Add files via upload

---
+++ pmg-gnn-vae/src/utils.py
@@ -0,0 +1,140 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+# training
+def training(name, max_patience, num_epochs, model, optimizer, training_loader, val_loader):
+    nll_val = []
+    best_nll = 1000.
+    patience = 0
+
+    # Main loop
+    for e in range(num_epochs):
+        # TRAINING
+        model.train()
+        for _, batch in enumerate(training_loader):
+            if hasattr(model, 'dequantization'):
+                if model.dequantization:
+                    batch = batch + torch.rand(batch.shape)
+            loss = model.forward(batch)
+
+            optimizer.zero_grad()
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+        # Validation
+        loss_val = evaluation(val_loader, model_best=model, epoch=e)
+        nll_val.append(loss_val)  # save for plotting
+
+        if e == 0:
+            print('saved!')
+            torch.save(model, name + '.model')
+            best_nll = loss_val
+        else:
+            if loss_val < best_nll:
+                print('saved!')
+                torch.save(model, name + '.model')
+                best_nll = loss_val
+                patience = 0
+
+                samples_generated(name, val_loader, extra_name="_epoch_" + str(e))
+            else:
+                patience = patience + 1
+
+        if patience > max_patience:
+            break
+
+    nll_val = np.asarray(nll_val)
+
+    return nll_val
+
+
+
+
+
+def evaluation(test_loader, name=None, model_best=None, epoch=None):
+    # EVALUATION
+    if model_best is None:
+        # load best performing model
+        model_best = torch.load(name + '.model')
+
+    model_best.eval()
+    loss = 0.
+    N = 0.
+    for _, test_batch in enumerate(test_loader):
+        loss_t = model_best.forward(test_batch, reduction='sum')
+        loss = loss + loss_t.item()
+        N = N + test_batch.shape[0]
+    loss = loss / N
+
+    if epoch is None:
+        print(f'FINAL LOSS: nll={loss}')
+    else:
+        print(f'Epoch: {epoch}, val nll={loss}')
+    return loss
+
+
+
+def samples_generated(name, data_loader, extra_name=''):
+    x = next(iter(data_loader)).detach().numpy()
+
+    # GENERATIONS-------
+    model_best = torch.load(name + '.model')
+    model_best.eval()
+
+    num_x = 2
+    num_y = 2
+    x = model_best.sample(num_x * num_y)
+    x = x.detach().numpy()
+
+    fig, ax = plt.subplots(num_x, num_y)
+    for i, ax in enumerate(ax.flatten()):
+        plottable_image = np.reshape(x[i], (32, 32, 3))
+        ax.imshow(plottable_image, cmap='gray')
+        ax.axis('off')
+
+    plt.savefig(name + '_generated_images' + extra_name + '.pdf', bbox_inches='tight')
+    plt.close()
+
+def samples_real(name, test_loader):
+    # REAL-------
+    num_x = 2
+    num_y = 2
+    x = next(iter(test_loader)).detach().numpy()
+
+    fig, ax = plt.subplots(num_x, num_y)
+    for i, ax in enumerate(ax.flatten()):
+        plottable_image = np.reshape(x[i], (32, 32, 3))
+        ax.imshow(plottable_image, cmap='gray')
+        ax.axis('off')
+
+    plt.savefig(name+'_real_images.pdf', bbox_inches='tight')
+    plt.close()
+
+
+def tsne_plot(name, data_loader, extra_name=""):
+    x = next(iter(data_loader)).detach().numpy()
+
+    # GENERATIONS-------
+    model_best = torch.load(name + '.model')
+    model_best.eval()
+
+    num_x = 4
+    num_y = 4
+    x = model_best.sample(num_x * num_y)
+    x = x.detach().numpy()
+    model_tsne = TSNE(n_components=2, random_state=0, perplexity=5).fit_transform(x)
+    fig = plt.figure()
+    plt.scatter(model_tsne[:, 0], model_tsne[:, 1])
+    plt.show()
+    plt.savefig(name + '_tsne.pdf', bbox_inches='tight')
+    plt.close()
+
+
+def plot_curve(name, nll_val):
+    plt.plot(np.arange(len(nll_val)), nll_val, linewidth='3')
+    plt.xlabel('epochs')
+    plt.ylabel('NLL')
+    plt.savefig(name + '_nll_val_curve.pdf', bbox_inches='tight')
+    plt.close()
